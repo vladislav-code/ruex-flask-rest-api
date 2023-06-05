e95b54daa135c5949b7c601a5f97c525f1b0ebf4
vladislav-code <vladoska75@gmail.com>
2023-06-06 01:46:52 +0300

    static test

--- models.py
+++ models.py
@@ -17,7 +17,7 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True) # unique ????
-    username = db.Column(db.String(80), unique=True, nullable=False) # необходимость unique
+    username = db.Column(db.String(80), nullable=False) # необходимость unique
     password_hash = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     phone_number = db.Column(db.String(20), unique=True, nullable=True) # необходимость unique
